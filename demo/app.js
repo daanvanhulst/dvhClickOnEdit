9709d4d2a9700a65c4a92e246453d4c353a71d5c
daanh <dvhulst@gmail.com>
2015-07-14 22:38:44 +0200

    Added onSave event to editText

--- demo/app.js
+++ demo/app.js
@@ -1,4 +1,19 @@
     var app = angular.module("sampleApp", ["dvhClickToEdit-tpl", "dvhClickToEdit"]);
     app.controller("SampleCtrl", function($scope) {
         $scope.text = "blablablabla";
+        $scope.textConfig = {
+            fieldType:"text",
+            value: $scope.text,
+            onSave: function(value) {
+                alert("value got saved: " + value)
+            }
+        };
+
+        $scope.textAreaConfig = {
+            fieldType:"textArea",
+            value: $scope.text,
+            onSave: function(value) {
+                alert("value got saved: " + value)
+            }
+        };
     });
--- src/dvhClickToEdit.ts
+++ src/dvhClickToEdit.ts
@@ -6,29 +6,45 @@
  */
 module ClickToEdit {
 
-    export interface IClickToEditScope extends ng.IScope {
+    interface IDvhConfig {
         fieldType: string;
         value: string;
+        onSave: ( value ) => void;
+    }
+
+    export interface IClickToEditScope extends ng.IScope {
+        dvhConfig: IDvhConfig;
+        value;
 		editMode: boolean;
+        saveValue: Function;
     }
 
     export class Element {
         // #region Angular directive properties, fields, and methods
-        public link: (scope: IClickToEditScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => void;
+        public link: ( scope: IClickToEditScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes ) => void;
         public scope = {
-            fieldType: "@",
-            value: "@"
+            dvhConfig: "="
         };
         // #endregion
 
         // #region Initialization and destruction
         constructor(editableDirectiveFactory, $compile) {
-            Element.prototype.link = (scope: IClickToEditScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes) => {
+            Element.prototype.link = ( scope: IClickToEditScope, element: ng.IAugmentedJQuery, attrs: ng.IAttributes ) => {
 
+                scope.value = scope.dvhConfig.value;
 				scope.editMode = false;
 
+                scope.saveValue = function(value) {
+                    scope.editMode = false;
+
+                    if (angular.isFunction(scope.dvhConfig.onSave)) {
+                        scope.dvhConfig.onSave(scope.value);
+                    }
+                    scope.dvhConfig.value = value;
+                };
+
                 // Create the editable element
-                var editableElement = editableDirectiveFactory.createEditableDirective(scope.fieldType);
+                var editableElement = editableDirectiveFactory.createEditableDirective(scope.dvhConfig.fieldType);
 
                 //Compile the editable element
                 var e = $compile(editableElement)(scope);
